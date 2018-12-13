c7e645820af281e639ac020f484058b60688e08f
Shridatta <sgz2@njit.edu>
2018-12-13 03:33:20 -0500

    added properties and methods

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -5,4 +5,21 @@ import { Component } from "@angular/core";
   templateUrl: "./user.component.html",
   styleUrls: ["./user.component.css"]
 })
-export class UserComponent {}
+export class UserComponent {
+  //properties
+  firstName = "Shridatta";
+  LastName = "Zanjare";
+  age = 25;
+
+  //methods
+  nextYear() {
+    this.age += 1;
+  }
+
+  constructor() {
+    console.log("constructor ran ");
+    console.log("age now " + this.age);
+    this.nextYear();
+    console.log("age next september " + this.age);
+  }
+}
